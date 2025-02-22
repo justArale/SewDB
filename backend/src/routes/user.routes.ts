ea8448c20809646b92fa7921a24c241a2a08ca72
justArale <just1arale@gmail.com>
2025-02-22 18:07:47 +0100

    Add PUT and DELETE routes for /users/:id, fix syntax to correctly retrieve user ID from params

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -3,6 +3,7 @@ import { Bindings } from "../bindings";
 import { users } from "../db/schema";
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import { eq } from "drizzle-orm";
 
 const userRouter = new Hono<{ Bindings: Bindings }>();
 
@@ -14,18 +15,74 @@ userRouter.get("/users", async (c) => {
   return c.json({ users: allUsers });
 });
 
+// GET /users/:id
 userRouter.get("/users/:id", async (c) => {
   const sql = neon(c.env.DATABASE_URL);
   const db = drizzle(sql);
 
-  const { id } = c.params;
+  const id = Number(c.req.param("id"));
 
-  const user = await db.select().from(users).where(users.id.eq(id)).limit(1);
+  const user = await db.select().from(users).where(eq(users.id, id)).limit(1);
 
   if (user.length === 0) {
     return c.json({ error: "User not found" }, 404);
   }
-  return c.json({ users: user[0] });
+
+  return c.json({ user: user[0] });
+});
+
+// PUT /users/:id
+userRouter.put("/users/:id", async (c) => {
+  const sql = neon(c.env.DATABASE_URL);
+  const db = drizzle(sql);
+  const id = Number(c.req.param("id"));
+  const { name, email, password, likes } = await c.req.json();
+
+  // User exists?
+  const userExists = await db
+    .select()
+    .from(users)
+    .where(eq(users.id, id))
+    .limit(1);
+  if (userExists.length === 0) {
+    return c.json({ error: "User not found" }, 404);
+  }
+
+  // Update user
+  const updatedUser = await db
+    .update(users)
+    .set({
+      name,
+      email,
+      password,
+      likes,
+      updatedAt: new Date(),
+    })
+    .where(eq(users.id, id));
+
+  return c.json({ user: updatedUser });
+});
+
+// DELETE /users/:id
+userRouter.delete("/users/:id", async (c) => {
+  const sql = neon(c.env.DATABASE_URL);
+  const db = drizzle(sql);
+  const id = Number(c.req.param("id"));
+
+  // User exists?
+  const userExists = await db
+    .select()
+    .from(users)
+    .where(eq(users.id, id))
+    .limit(1);
+  if (userExists.length === 0) {
+    return c.json({ error: "User not found" }, 404);
+  }
+
+  // Delete the user
+  await db.delete(users).where(eq(users.id, id));
+
+  return c.json({ message: "User deleted successfully" });
 });
 
 export default userRouter;
