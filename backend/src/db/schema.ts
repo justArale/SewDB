80c0dd97a93bb9ecf0c4b82a476a2e5cff499f4c
justArale <just1arale@gmail.com>
2025-02-22 19:26:45 +0100

    Update user schema with necessary parameters, add schema for the pattern table

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,12 +1,25 @@
 import { jsonb, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
 export type NewUser = typeof users.$inferInsert;
+export type NewPattern = typeof patterns.$inferInsert;
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   name: text("name"),
   email: text("email"),
-  settings: jsonb("settings"),
+  password: text("password"),
+  likes: jsonb("likes"),
+  image: text("image"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const patterns = pgTable("patterns", {
+  id: serial("id").primaryKey(),
+  name: text("name"),
+  image: text("image"),
+  category: jsonb("category"),
+  sizes: jsonb("sizes"),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
