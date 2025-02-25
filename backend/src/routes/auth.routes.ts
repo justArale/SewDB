c87bacec6eb2bd4e711618474fc6ef6662cc3535
justArale <just1arale@gmail.com>
2025-02-25 18:52:14 +0100

    Set route for signup and login

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -4,124 +4,131 @@ import { users } from "../db/schema";
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import { eq } from "drizzle-orm";
-// @ts-ignore
-import bcrypt from "bcrypt";
-// @ts-ignore
-import jwt from "jsonwebtoken";
-// import { isAuthenticated } from "../../middleware/jwt.middleware";
-
-// interface CustomContext {
-//   get(key: "payload"): any;
-// }
+import { logger } from "hono/logger";
+import { z } from "zod";
+import { sign } from "hono/jwt";
+import { getCookie, setCookie } from "hono/cookie";
+import bcrypt from "bcryptjs";
 
 const authRouter = new Hono<{ Bindings: Bindings }>();
-const saltRounds = 10;
 
-// authRouter.post("/signup", async (c) => {
-//   try {
-//     const { email, password, name } = await c.req.json();
+authRouter.use(logger());
 
-//     if (!email || !password || !name) {
-//       return c.json({ message: "Provide email, password and name" }, 400);
-//     }
+const signupSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+  name: z.string(),
+});
 
-//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
-//     if (!emailRegex.test(email)) {
-//       return c.json({ message: "Provide a valid email address." }, 400);
-//     }
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
 
-//     const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-//     if (!passwordRegex.test(password)) {
-//       return c.json(
-//         {
-//           message:
-//             "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
-//         },
-//         400
-//       );
-//     }
+authRouter.post("/signup", async (c) => {
+  const { email, password, name } = signupSchema.parse(await c.req.json());
 
-//     // Datenbankverbindung
-//     const sql = neon(c.env.DATABASE_URL);
-//     const db = drizzle(sql);
+  if (!email || !password || !name) {
+    return c.json({ message: "Provide email, password and name" }, 400);
+  }
 
-//     // Prüfe, ob der Nutzer existiert
-//     const foundUser = await db
-//       .select()
-//       .from(users)
-//       .where(eq(users.email, email))
-//       .execute();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+  if (!emailRegex.test(email)) {
+    return c.json({ message: "Provide a valid email address." }, 400);
+  }
 
-//     if (foundUser.length > 0) {
-//       return c.json({ message: "User already exists." }, 400);
-//     }
+  const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!passwordRegex.test(password)) {
+    return c.json(
+      {
+        message:
+          "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
+      },
+      400
+    );
+  }
+  const sql = neon(c.env.DATABASE_URL);
+  const db = drizzle(sql);
 
-//     // Hash das Passwort und erstelle den Nutzer
-//     const salt = await bcrypt.genSalt(saltRounds);
-//     const hashedPassword = await bcrypt.hash(password, salt);
+  const foundUser = await db
+    .select()
+    .from(users)
+    .where(eq(users.email, email))
+    .execute();
 
-//     const createdUser = await db
-//       .insert(users)
-//       .values({
-//         email,
-//         password: hashedPassword,
-//         name,
-//       })
-//       .returning();
+  if (foundUser.length > 0) {
+    return c.json({ message: "User already exists." }, 400);
+  }
 
-//     return c.json({ user: createdUser[0] }, 201);
-//   } catch (err) {
-//     console.error("Error during signup:", err);
-//     return c.json({ message: "Server error" }, 500);
-//   }
-// });
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-// authRouter.post("/login", async (c) => {
-//   try {
-//     const { email, password } = await c.req.json();
+  try {
+    const createdUser = await db
+      .insert(users)
+      .values({
+        email,
+        password: hashedPassword,
+        name,
+      })
+      .returning();
 
-//     if (!email || !password) {
-//       return c.json({ message: "Provide email and password." }, 400);
-//     }
+    return c.json({ user: createdUser[0] }, 201);
+  } catch (err) {
+    console.error("Error during signup:", err);
+    return c.json({ message: "Server error" }, 500);
+  }
+});
 
-//     const sql = neon(c.env.DATABASE_URL);
-//     const db = drizzle(sql);
+authRouter.post("/login", async (c) => {
+  try {
+    const { email, password } = loginSchema.parse(await c.req.json());
 
-//     const foundUser = await db
-//       .select()
-//       .from(users)
-//       .where(eq(users.email, email))
-//       .execute();
+    if (!email || !password) {
+      return c.json({ message: "Provide email and password." }, 400);
+    }
 
-//     if (foundUser.length === 0) {
-//       return c.json({ message: "User not found." }, 401);
-//     }
+    const sql = neon(c.env.DATABASE_URL);
+    const db = drizzle(sql);
 
-//     const user = foundUser[0];
+    const foundUser = await db
+      .select()
+      .from(users)
+      .where(eq(users.email, email))
+      .execute();
 
-//     const passwordCorrect = await bcrypt.compare(password, user.password);
+    if (foundUser.length === 0) {
+      return c.json({ message: "User not found." }, 401);
+    }
 
-//     if (passwordCorrect) {
-//       const payload = { id: user.id, email: user.email, name: user.name };
+    const user = foundUser[0];
 
-//       const authToken = jwt.sign(payload, process.env.TOKEN_SECRET!, {
-//         algorithm: "HS256",
-//         expiresIn: "6h",
-//       });
+    const passwordCorrect = await bcrypt.compare(password, user.password);
 
-//       return c.json({ authToken });
-//     } else {
-//       return c.json({ message: "Unable to authenticate the user" }, 401);
-//     }
-//   } catch (err) {
-//     console.error("Error during login:", err);
-//     return c.json({ message: "Server error" }, 500);
-//   }
-// });
+    if (passwordCorrect) {
+      const payload = {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        exp: Math.floor(Date.now() / 1000) + 6 * 60,
+      };
+
+      const authToken = await sign(payload, c.env.TOKEN_SECRET);
+      setCookie(c, "authToken", authToken);
+
+      return c.json({ authToken });
+    } else {
+      return c.json({ message: "Unable to authenticate the user" }, 401);
+    }
+  } catch (err) {
+    console.error("Error during login:", err);
+    return c.json({ message: "Server error" }, 500);
+  }
+});
 
 // 🚀 Verify Route mit Auth Middleware
 // authRouter.get("/verify", isAuthenticated, async (c) => {
-//   const payload = c.get("payload"); // Prüfen, ob das Payload existiert
+//   const payload = c.get("payload");
 //   if (!payload) {
 //     return c.json({ message: "Unauthorized" }, 401);
 //   }
