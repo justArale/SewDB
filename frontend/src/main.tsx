cfcb6bcbd7447267450ef6b324a87a63ff674aec
justArale <just1arale@gmail.com>
2025-02-28 09:43:54 +0100

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,13 @@
-import { StrictMode } from "react";
 import "./index.css";
 import App from "./App.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </StrictMode>
+  </React.StrictMode>
 );
