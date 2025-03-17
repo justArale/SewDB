de3ecb128f30e35138bbd1dd0ddec6538f2fd261
justArale <just1arale@gmail.com>
2025-03-17 19:34:30 +0100

    Add memoji of me, set a third box with it an the link to my portfolio

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { GitHub, LinkedIn } from "@just1arale/icons";
 import "../components/AboutPage.css";
+import aralememoji from "../assets/image/aralememoji.svg";
 
 const AboutPage: React.FC = () => {
   return (
@@ -12,13 +13,12 @@ const AboutPage: React.FC = () => {
           patterns. Think of it as my personal sewing database, where patterns
           can be browsed, filtered, and liked. SewDB is mainly for my friends—so
           when they ask me to sew something, we can easily find the perfect
-          pattern together. But honestly? This is more than just a collection.
-          It's where my past career, my love for sewing, and my dream of working
-          in tech come together. A little bit of code, a little bit of
-          creativity, and a whole lot of passion stitched into one. To respect
-          copyright rules, SewDB is a private space. You'll need to log in to
-          access everything, but once you're in, feel free to explore and get
-          inspired!
+          pattern together. But - this is more than just a collection. It's
+          where my past career, my love for sewing, and my dream of working in
+          tech come together. A little bit of code, a little bit of creativity,
+          and a whole lot of passion stitched into one. To respect copyright
+          rules, SewDB is a private space. You'll need to log in to access
+          everything, but once you're in, feel free to explore and get inspired!
         </p>
       </div>
       <div className="social-pages">
@@ -34,6 +34,12 @@ const AboutPage: React.FC = () => {
             href="https://www.linkedin.com/in/s-kuechler-jr-fullstack-dev"
           >
             <LinkedIn width="32" height="32" color="#000" />
+          </a>
+        </button>
+        <button className=" noUnderline socialButton linkedIn">
+          <a target="_blank" href="https://portfolio.arale.space">
+            {/* <LinkedIn width="32" height="32" color="#000" /> */}
+            <img src={aralememoji} width="32" height="32" />
           </a>
         </button>
       </div>
