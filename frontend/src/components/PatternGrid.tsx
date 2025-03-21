9c12eac9d9016e4ccbb5e602dd82f6d89b40295a
justArale <just1arale@gmail.com>
2025-03-21 12:28:53 +0100

    Styling of the errorpage

--- frontend/src/components/PatternGrid.tsx
+++ frontend/src/components/PatternGrid.tsx
@@ -1,4 +1,3 @@
-// src/components/AllPatterns.tsx
 import React from "react";
 import { Link } from "react-router-dom";
 import { Pattern } from "../service/pattern.service";
--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-const imgURL =
-  "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbno3cmxtYWQyeXU0MWlvcThpdndiMnh3MXY0am83emx1NzcwdWE3OCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/C21GGDOpKT6Z4VuXyn/giphy.gif";
 
 const ErrorPage: React.FC = () => {
   return (
-    <div>
-      <img src={imgURL} alt="404 error gif" className="page-img" />
+    <div className="errorPage">
+      <h4 className="pagetitle">Oops, Wrong Turn ...</h4>
+      <p className="bodyfont">
+        Lost in the pattern maze? This page doesn’t seem to exist, but there are
+        plenty of designs waiting for you. Let’s get you back.
+      </p>
+      <button className="buttonAction labelfont">Back to Home</button>
     </div>
   );
 };
