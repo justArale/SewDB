dc6595dd4b1e67a064d11d879169a004eb1a3ed8
justArale <just1arale@gmail.com>
2025-03-26 14:12:34 +0100

    Add link back to the dashboard to the 'Back to Home'-button on the errorpage

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage: React.FC = () => {
   return (
@@ -8,7 +9,9 @@ const ErrorPage: React.FC = () => {
         Lost in the pattern maze? This page doesn’t seem to exist, but there are
         plenty of designs waiting for you. Let’s get you back.
       </p>
-      <button className="buttonAction labelfont">Back to Home</button>
+      <Link to="/">
+        <button className="buttonAction labelfont">Back to Home</button>
+      </Link>
     </div>
   );
 };
