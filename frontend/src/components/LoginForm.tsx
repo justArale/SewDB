b522464017960b3b6b3ef96224c32db94557c2d7
justArale <just1arale@gmail.com>
2025-04-22 12:44:26 +0200

    Cleanup: Remove first div-container because its useless

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -20,60 +20,58 @@ const LoginForm: React.FC<LoginFormProps> = ({
   onSwitch,
 }) => {
   return (
-    <div>
-      <div className="access">
-        <form onSubmit={handleLoginSubmit} className="accessForm">
-          <h3 className="bodyfontLarge">Log In</h3>
+    <div className="access">
+      <form onSubmit={handleLoginSubmit} className="accessForm">
+        <h3 className="bodyfontLarge">Log In</h3>
 
-          <div className="input-group">
-            <label htmlFor="email" className="bodyfont secondaryFontColor">
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={email}
-              onChange={handleEmail}
-              autoComplete="off"
-              placeholder="What's your email?"
-              className="accessInput"
-            />
-          </div>
+        <div className="input-group">
+          <label htmlFor="email" className="bodyfont secondaryFontColor">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={handleEmail}
+            autoComplete="off"
+            placeholder="What's your email?"
+            className="accessInput"
+          />
+        </div>
 
-          <div className="input-group">
-            <label htmlFor="password" className="bodyfont secondaryFontColor">
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              value={password}
-              onChange={handlePassword}
-              autoComplete="off"
-              placeholder="••••••••••••••••"
-              className="accessInput"
-            />
-          </div>
+        <div className="input-group">
+          <label htmlFor="password" className="bodyfont secondaryFontColor">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={handlePassword}
+            autoComplete="off"
+            placeholder="••••••••••••••••"
+            className="accessInput"
+          />
+        </div>
 
-          <button type="submit" className="buttonfont buttonSave">
-            Log In
-          </button>
-          <p className="bodyfont secondaryFontColor">
-            Don’t have an account?{" "}
-            <a
-              href="#"
-              onClick={onSwitch}
-              className="noUnderline secondaryFontColor"
-            >
-              Sign Up
-            </a>
-          </p>
-        </form>
+        <button type="submit" className="buttonfont buttonSave">
+          Log In
+        </button>
+        <p className="bodyfont secondaryFontColor">
+          Don’t have an account?{" "}
+          <a
+            href="#"
+            onClick={onSwitch}
+            className="noUnderline secondaryFontColor"
+          >
+            Sign Up
+          </a>
+        </p>
+      </form>
 
-        {errorMessage && <p className="bodyfont">{errorMessage}</p>}
-      </div>
+      {errorMessage && <p className="bodyfont">{errorMessage}</p>}
     </div>
   );
 };
