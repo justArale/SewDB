6da305467141cb6bff0ef7fe757f24b5f0991dc4
justArale <just1arale@gmail.com>
2025-04-23 16:47:12 +0200

    Add Array.isArray before iterating through the pattern to make sure, that it is an array

--- frontend/src/components/PatternGrid.tsx
+++ frontend/src/components/PatternGrid.tsx
@@ -19,37 +19,38 @@ const AllPattern: React.FC<PatternProps> = ({ patterns }) => {
 
   return (
     <div className="patternGrid">
-      {patterns.map((pattern) => (
-        <div key={pattern.id} className="patternCard">
-          <Link to={`/patterns/${pattern.id}`} className="noUnderline">
-            {pattern.image && (
-              <img
-                src={pattern.image[0]}
-                alt={pattern.name}
-                className="patternCardImage"
-              />
-            )}
-            <div className="patternCardContent primaryFontColor">
-              <h2 className="labelfont">{pattern.name}</h2>
-              <p className="bodyfont">Sizes: {pattern.sizes.join(", ")}</p>
-            </div>
-          </Link>
-          <span
-            className="likeIconWrapper"
-            onClick={() => {
-              if (user?.id && pattern.id) {
-                handleLikeClick(pattern.id, user.id);
-              }
-            }}
-          >
-            {pattern.id && isPatternLiked(pattern.id) ? (
-              <img src={HearFill} alt="heart" />
-            ) : (
-              <img src={HearUnfill} alt="heart" />
-            )}
-          </span>
-        </div>
-      ))}
+      {Array.isArray(patterns) &&
+        patterns.map((pattern) => (
+          <div key={pattern.id} className="patternCard">
+            <Link to={`/patterns/${pattern.id}`} className="noUnderline">
+              {pattern.image && (
+                <img
+                  src={pattern.image[0]}
+                  alt={pattern.name}
+                  className="patternCardImage"
+                />
+              )}
+              <div className="patternCardContent primaryFontColor">
+                <h2 className="labelfont">{pattern.name}</h2>
+                <p className="bodyfont">Sizes: {pattern.sizes.join(", ")}</p>
+              </div>
+            </Link>
+            <span
+              className="likeIconWrapper"
+              onClick={() => {
+                if (user?.id && pattern.id) {
+                  handleLikeClick(pattern.id, user.id);
+                }
+              }}
+            >
+              {pattern.id && isPatternLiked(pattern.id) ? (
+                <img src={HearFill} alt="heart" />
+              ) : (
+                <img src={HearUnfill} alt="heart" />
+              )}
+            </span>
+          </div>
+        ))}
     </div>
   );
 };
