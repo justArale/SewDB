9392b95dc35d77759ed98ab44c95e9ca70a221b1
justArale <just1arale@gmail.com>
2025-05-21 12:03:31 +0200

    Create the page for the visible response of verifying the user token from the email

---
+++ frontend/src/pages/UserVerifyPage.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { verifyUser } from "../service/user.service";
+
+const UserVerifyPage: React.FC = () => {
+  const [status, setStatus] = useState<"loading" | "success" | "error">(
+    "loading"
+  );
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get("token");
+
+    if (token) {
+      verifyUser(token)
+        .then((response) => {
+          console.log("response", response);
+          if (response && response.message === "successfully") {
+            setStatus("success");
+            setTimeout(() => {
+              window.location.href = "/";
+            }, 1500);
+          } else {
+            setStatus("error");
+          }
+        })
+        .catch((error) => {
+          console.error("Error verifying user:", error);
+          setStatus("error");
+        });
+    } else {
+      setStatus("error");
+    }
+  }, []);
+
+  return (
+    <div>
+      {status === "loading" && <p>Verifing user...</p>}
+      {status === "success" && <p>Success! Returning to main page.</p>}
+      {status === "error" && <p>An error occurred. Please try again.</p>}
+    </div>
+  );
+};
+
+export default UserVerifyPage;
